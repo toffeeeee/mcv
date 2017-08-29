da08f719295babb6884a3e4a17f7b2a79d391c3d
1iekkas <764295062@qq.com>
2017-08-29 17:11:39 +0800

    1.0.0

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,77 @@ export default {
       transitionName:'slide-left'
     }
   },
+  mounted () {
+    this.touchMove ();
+  },
+  methods:{
+    touchMove () {
+      var startX = 0
+      var startY = 0
+        function touchSatrtFunc(evt) {
+                  try{
+                      //evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+
+                      var touch = evt.touches[0]; //获取第一个触点
+                      var x = Number(touch.pageX); //页面触点X坐标
+                      var y = Number(touch.pageY); //页面触点Y坐标
+                      //记录触点初始位置
+                      startX = x;
+                      startY = y;
+
+                      var text = 'TouchStart事件触发：（' + x + ', ' + y + '）';
+                      alert(text);
+                  }
+                  catch (e) {
+                      alert('touchSatrtFunc：' + e.message);
+                  }
+        }
+
+        function touchMoveFunc(evt) {
+                  try
+                  {
+                      //evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+                      var touch = evt.touches[0]; //获取第一个触点
+                      var x = Number(touch.pageX); //页面触点X坐标
+                      var y = Number(touch.pageY); //页面触点Y坐标
+
+                      var text = 'TouchMove事件触发：（' + x + ', ' + y + '）';
+
+                      //判断滑动方向
+                      if (x - startX != 0) {
+                          text += '<br/>左右滑动';
+                      }
+                      if (y - startY != 0) {
+                          text += '<br/>上下滑动';
+                      }
+
+                      alert(text)
+                  }
+                  catch (e) {
+                      alert('touchMoveFunc：' + e.message);
+                  }
+              }
+              function touchEndFunc(evt) {
+                      try {
+                          //evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+
+                          var text = 'TouchEnd事件触发';
+                        alert(text)
+                      }
+                      catch (e) {
+                          alert('touchEndFunc：' + e.message);
+                      }
+                }
+
+      function bindEvent() {
+                document.addEventListener('touchstart', touchSatrtFunc, false);
+                document.addEventListener('touchmove', touchMoveFunc, false);
+                document.addEventListener('touchend', touchEndFunc, false);
+            }
+
+
+    }
+  },
   watch: {
     '$route' (to, from) {
       let isBack = this.$router.isBack
